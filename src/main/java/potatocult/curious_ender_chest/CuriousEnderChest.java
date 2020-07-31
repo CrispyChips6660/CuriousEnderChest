c168a5d7e95fda65829b5871f5d4cc8655bfdc46
cripsy_chips1234 <60465425+CrispyChips6660@users.noreply.github.com>
2020-07-31 11:33:54 +0800

    Broke the Keybind

--- src/main/java/potatocult/curious_ender_chest/CuriousEnderChest.java
+++ src/main/java/potatocult/curious_ender_chest/CuriousEnderChest.java
@@ -1,12 +1,23 @@
 package potatocult.curious_ender_chest;
 
+import net.minecraft.block.Blocks;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.settings.KeyBinding;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.inventory.container.ChestContainer;
+import net.minecraft.inventory.container.SimpleNamedContainerProvider;
+import net.minecraft.item.Item;
+import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraftforge.client.event.InputEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.network.NetworkHooks;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import top.theillusivec4.curios.api.CuriosAPI;
 
 
 import static org.lwjgl.glfw.GLFW.GLFW_KEY_F;
@@ -27,6 +38,15 @@ public class CuriousEnderChest {
 
     @SubscribeEvent
     public void EnderchestKeyDetection(InputEvent.KeyInputEvent event){
-
+        //Todo: fix dat ClassCastException
+        if(CuriousEnderChest.OPEN_ENDERCHEST.isKeyDown()){
+            PlayerEntity player_entity = Minecraft.getInstance().player;
+            if (CuriosAPI.getCurioEquipped(Item.getItemFromBlock(Blocks.ENDER_CHEST), (LivingEntity) player_entity).isPresent()) {
+                    ServerPlayerEntity player_ec = (ServerPlayerEntity) player_entity;
+                    NetworkHooks.openGui(player_ec, new SimpleNamedContainerProvider((id, playerInventory, player) -> {
+                        return ChestContainer.createGeneric9X3(id, playerInventory, player_ec.getInventoryEnderChest());
+                    }, new TranslationTextComponent("container.enderchest")));
+            }
+        }
     }
 }
