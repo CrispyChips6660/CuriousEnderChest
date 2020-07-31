55da09685140f07bb8160f505c99d961d2f0a7ee
cripsy_chips1234 <60465425+CrispyChips6660@users.noreply.github.com>
2020-07-31 11:52:36 +0800

    Cleaned up code

--- src/main/java/potatocult/curious_ender_chest/CuriousEnderChest.java
+++ src/main/java/potatocult/curious_ender_chest/CuriousEnderChest.java
@@ -1,11 +1,10 @@
 package potatocult.curious_ender_chest;
 
-import net.minecraft.block.Blocks;
 import net.minecraft.client.settings.KeyBinding;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.inventory.container.ChestContainer;
 import net.minecraft.inventory.container.SimpleNamedContainerProvider;
-import net.minecraft.item.Item;
+import net.minecraft.item.Items;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraftforge.client.event.InputEvent;
 import net.minecraftforge.common.MinecraftForge;
@@ -17,12 +16,15 @@ import top.theillusivec4.curios.api.CuriosAPI;
 
 import static org.lwjgl.glfw.GLFW.GLFW_KEY_F;
 
-// The value here should match an entry in the META-INF/mods.toml file
+/***
+ * @since 1.o
+ * @author crispy_chips1234
+ */
 @Mod("curious_ender_chest")
 public class CuriousEnderChest {
+    public static void main(String[] args) {
+    }
 
-    // Directly reference a log4j logger.
-    //private static final Logger LOGGER = LogManager.getLogger();
     public static KeyBinding OPEN_ENDERCHEST = new KeyBinding("key.curious_ender_chest.open_enderchest.desc", GLFW_KEY_F, "key.categories.gameplay");
 
     public CuriousEnderChest() {
@@ -37,7 +39,7 @@ public class CuriousEnderChest {
 
     public static void OpenEnderChestGUI(ServerPlayerEntity entity) {
         if (CuriousEnderChest.OPEN_ENDERCHEST.isKeyDown()) {
-            if (CuriosAPI.getCurioEquipped(Item.getItemFromBlock(Blocks.ENDER_CHEST), entity).isPresent()) {
+            if (CuriosAPI.getCurioEquipped(Items.ENDER_CHEST, entity).isPresent()) {
                 NetworkHooks.openGui(entity, new SimpleNamedContainerProvider((id, playerInventory, player) -> {
                     return ChestContainer.createGeneric9X3(id, playerInventory, entity.getInventoryEnderChest());
                 }, new TranslationTextComponent("container.enderchest")));
