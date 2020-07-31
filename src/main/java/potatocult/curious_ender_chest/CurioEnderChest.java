6849e59c33b5b7470903ce91a419fff96d80d87a
cripsy_chips1234 <60465425+CrispyChips6660@users.noreply.github.com>
2020-07-31 12:34:06 +0800

    Add the curio capability

---
+++ src/main/java/potatocult/curious_ender_chest/CurioEnderChest.java
@@ -0,0 +1,42 @@
+package potatocult.curious_ender_chest;
+
+import com.mojang.blaze3d.matrix.MatrixStack;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.IRenderTypeBuffer;
+import net.minecraft.client.renderer.model.ItemCameraTransforms;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Direction;
+import top.theillusivec4.curios.api.capability.ICurio;
+
+public class CurioEnderChest implements ICurio {
+
+    private final ItemStack displayedItem;
+
+    public CurioEnderChest(ItemStack item) {
+        this.displayedItem = item;
+    }
+
+    @Override
+    public boolean hasRender(String identifier, LivingEntity livingEntity) {
+        return true;
+    }
+
+    @Override
+    public void render(String identifier, MatrixStack matrixStack, IRenderTypeBuffer renderTypeBuffer, int light, LivingEntity livingEntity, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch) {
+        ICurio.RenderHelper.translateIfSneaking(matrixStack, livingEntity);
+        ICurio.RenderHelper.rotateIfSneaking(matrixStack, livingEntity);
+        matrixStack.push();
+        matrixStack.translate(0D, 0.35D, 0D);
+        matrixStack.scale(2f, 2f, 1.5f);
+        matrixStack.rotate(Direction.SOUTH.getRotation());
+        Minecraft.getInstance().getFirstPersonRenderer().renderItemSide(livingEntity, this.displayedItem, ItemCameraTransforms.TransformType.GROUND, false, matrixStack, renderTypeBuffer, light);
+        matrixStack.pop();
+    }
+
+    @Override
+    public boolean canRightClickEquip() {
+        return true;
+    }
+}
+
--- src/main/java/potatocult/curious_ender_chest/CuriousEnderChest.java
+++ src/main/java/potatocult/curious_ender_chest/CuriousEnderChest.java
@@ -1,20 +1,37 @@
 package potatocult.curious_ender_chest;
 
+import net.minecraft.block.Block;
+import net.minecraft.block.EnderChestBlock;
 import net.minecraft.client.settings.KeyBinding;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.inventory.container.ChestContainer;
 import net.minecraft.inventory.container.SimpleNamedContainerProvider;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.util.Direction;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraftforge.client.event.InputEvent;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.capabilities.ICapabilityProvider;
+import net.minecraftforge.common.util.LazyOptional;
+import net.minecraftforge.event.AttachCapabilitiesEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.network.NetworkHooks;
 import potatocult.curious_ender_chest.networking.EnderChestPacket;
 import potatocult.curious_ender_chest.networking.NetworkLoader;
 import top.theillusivec4.curios.api.CuriosAPI;
+import top.theillusivec4.curios.api.capability.CuriosCapability;
+import top.theillusivec4.curios.api.capability.ICurio;
+import top.theillusivec4.curios.api.imc.CurioIMCMessage;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 
 import static org.lwjgl.glfw.GLFW.GLFW_KEY_F;
 
@@ -23,6 +40,7 @@ import static org.lwjgl.glfw.GLFW.GLFW_KEY_F;
  */
 @Mod("curious_ender_chest")
 public class CuriousEnderChest {
+
     public static void main(String[] args) {
     }
 
@@ -31,8 +49,30 @@ public class CuriousEnderChest {
     public CuriousEnderChest() {
         MinecraftForge.EVENT_BUS.register(this);
         ClientRegistry.registerKeyBinding(OPEN_ENDERCHEST);
-
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
         NetworkLoader.registerMessages();
+        MinecraftForge.EVENT_BUS.addGenericListener(ItemStack.class, this::attachCapabilities);
+    }
+
+    @SubscribeEvent
+    public void attachCapabilities(AttachCapabilitiesEvent<ItemStack> event) {
+        ItemStack stack = event.getObject();
+        Block block = EnderChestBlock.getBlockFromItem(stack.getItem());
+        CurioEnderChest curioEnderChest = new CurioEnderChest(stack);
+        event.addCapability(CuriosCapability.ID_ITEM, new ICapabilityProvider() {
+            final LazyOptional<ICurio> curio = LazyOptional.of(() -> curioEnderChest);
+
+            @Nonnull
+            @Override
+            public <T> LazyOptional<T> getCapability(@Nonnull Capability<T> cap,
+                                                     @Nullable Direction side) {
+                return CuriosCapability.ITEM.orEmpty(cap, curio);
+            }
+        });
+    }
+
+    private void enqueueIMC(final InterModEnqueueEvent event) {
+        InterModComms.sendTo(CuriosAPI.MODID, CuriosAPI.IMC.REGISTER_TYPE, () -> new CurioIMCMessage("back").setSize(1).setEnabled(true).setHidden(false));
     }
 
     @SubscribeEvent
